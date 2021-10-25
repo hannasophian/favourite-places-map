470bce11536b4773c4c100ed5a963927b1926b69
Hanna <sophianhanna@gmail.com>
2021-10-25 11:19:03 +0100

    Added Header

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,10 @@
+function AppHeader(): JSX.Element {
+    return (
+        <header>
+            <h1>My Favourite Places</h1>
+            <h3>by Hanna Sophian</h3>
+        </header> 
+    )
+}
+
+export default AppHeader;
